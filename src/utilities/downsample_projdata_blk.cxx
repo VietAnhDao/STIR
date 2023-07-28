6ae1fb2cef81c447072bbf011d2f3bfca4d0ebf6
VietAnhDao <1997vietdao@gmail.com>
2023-07-28 16:25:27 +0200

    no seg fault

--- src/utilities/downsample_projdata_blk.cxx
+++ src/utilities/downsample_projdata_blk.cxx
@@ -6,6 +6,7 @@
 #include "stir/extend_boost.h" 
 #include "stir/DetectionPosition.h" 
 #include "stir/DetectionPositionPair.h" 
+#include "stir/Sinogram.h"
 #include "stir/Bin.h" 
 #include "stir/LORCoordinates.h" 
 #include <boost/format.hpp> 
@@ -37,9 +38,11 @@ int main(int argc, char * argv[]){
     float new_det_spacing = float(old_scanner_ptr->get_transaxial_crystal_spacing()*downsample_detector_scale); 
     new_scanner_sptr->set_num_detectors_per_ring(new_num_detector); 
     new_scanner_sptr->set_transaxial_crystal_spacing(new_det_spacing); 
+
     float new_num_arccorrected_bins = new_scanner_sptr->get_default_num_arccorrected_bins()/downsample_detector_scale + 5; 
     new_scanner_sptr->set_default_num_arccorrected_bins(ceil(new_num_arccorrected_bins + 0.5f)); 
     new_scanner_sptr->set_max_num_non_arccorrected_bins(ceil(new_num_arccorrected_bins + 0.5f)); 
+
     new_scanner_sptr->set_default_bin_size(new_scanner_sptr->get_effective_ring_radius() * _PI / new_num_detector); 
     new_scanner_sptr->set_num_transaxial_crystals_per_block(old_scanner_ptr->get_num_transaxial_crystals_per_block()/downsample_detector_scale); 
     int new_num_ring = int(old_scanner_ptr->get_num_rings()/downsample_ring_scale); 
@@ -56,9 +59,6 @@ int main(int argc, char * argv[]){
                                                             new_scanner_sptr->get_num_detectors_per_ring()/2, 
                                                             new_scanner_sptr->get_max_num_non_arccorrected_bins(), 
                                                             false)); 
-    // now we cast the ProjDataInfo down to either ProjDataInfoCylindrical/ProjDataInfoBlocksOnCylindrical/ProjDataInfoGeneric. 
-    
-    
     // create a crystal map between full res and low res 
     boost::coord_map<uint, uint> full_res_to_low_res_map; 
     uint full_res_det_num = old_scanner_ptr->get_num_detectors_per_ring(); 
@@ -78,74 +78,118 @@ int main(int argc, char * argv[]){
             } 
         } 
     } 
+    std::cout << "down casting projdata info\n";
     // cast the proj_data_info down to the appropriate child class
     const ProjDataInfoBlocksOnCylindricalNoArcCorr * full_res_proj_data_info_blk_ptr =
         dynamic_cast<const ProjDataInfoBlocksOnCylindricalNoArcCorr *>(full_res_proj_data_info_sptr.get());
     const ProjDataInfoBlocksOnCylindricalNoArcCorr * low_res_proj_data_info_blk_ptr =
         dynamic_cast<const ProjDataInfoBlocksOnCylindricalNoArcCorr *>(downsample_proj_data_info_sptr.get());
+    // create segment and load segment into memory
     VectorWithOffset<segment_type *> 
     low_res_segments (low_res_proj_data_info_blk_ptr->get_min_segment_num(), 
 	      low_res_proj_data_info_blk_ptr->get_max_segment_num());
     int start_segment_index = low_res_proj_data_info_blk_ptr->get_min_segment_num();
     int end_segment_index = low_res_proj_data_info_blk_ptr->get_max_segment_num();
+    
     for (int seg=start_segment_index ; seg<=end_segment_index; seg++)
     {
-        low_res_segments[seg] = new SegmentByView<elem_type>(low_res_proj_data_info_blk_ptr->get_empty_segment_by_view (seg));
+        low_res_segments[seg] = new SegmentByView<elem_type>(downsample_proj_data_info_sptr->get_empty_segment_by_view(seg));
+        for(int view = downsample_proj_data_info_sptr->get_min_view_num(); view <= downsample_proj_data_info_sptr->get_max_view_num(); view++){
+            for(int ax = downsample_proj_data_info_sptr->get_min_axial_pos_num(seg); ax <= downsample_proj_data_info_sptr->get_max_axial_pos_num(seg); ax++){
+                for(int tang = downsample_proj_data_info_sptr->get_min_tangential_pos_num(); tang <= downsample_proj_data_info_sptr->get_max_tangential_pos_num(); tang++){
+                    (*low_res_segments[seg])[view][ax][tang] = 0.f;
+                    //std::cout << seg << ",\t" << view << ",\t" << ax << ",\t" << tang << ",\t" << (*low_res_segments[seg])[view][ax][tang] << "\n";
+                }
+            }
+        }
     }
+    // create a new ProjData to stores data
+    shared_ptr<ExamInfo> exam_info_sptr(new ExamInfo); 
+    shared_ptr<ProjData> downsample_projdata_sptr; 
+    downsample_projdata_sptr.reset(new ProjDataInterfile(exam_info_sptr, 
+    low_res_proj_data_info_blk_ptr->create_shared_clone(), 
+    "test_downsample.hs", 
+    std::ios::in | std::ios::out | std::ios::trunc)); 
+    for (int seg=start_segment_index; seg<=end_segment_index; seg++)
+    {
+        downsample_projdata_sptr->set_segment(*low_res_segments[seg]);
+    }
+
     // now loop through all possible LORS in full res and combine the count to low res scanner. 
     stir::DetectionPositionPair<> full_res_det_pos_pair; 
     stir::DetectionPositionPair<> low_res_det_pos_pair; 
+    std::cout << "loading full res sinogram\n";
+    Sinogram<float> full_res_sino = full_resolution_proj_data_sptr->get_sinogram(0, 0);
+    std::cout << "looping thorugh all bins\n";
+
+    /*
+    uint low_res_det_num = new_scanner_ptr->get_num_detectors_per_ring(); 
+    uint low_res_ring_num = new_scanner_ptr->get_num_rings(); 
+    uint low_res_layer_num = new_scanner_ptr->get_num_detector_layers(); 
+
+    for(uint ringA=0; ringA < low_res_ring_num; ringA++){
+        for(uint detA=0; detA < low_res_det_num; detA++){
+            for(uint ringB=0; ringB < low_res_ring_num; ringB++){
+                for (uint detB = 0; detB < low_res_ring_num; detB++)
+                {
+                    
+                }
+            }
+        }
+    }
+    */
     for(uint ringA = 0; ringA < full_res_ring_num; ringA++) 
     { 
         for (uint detA = 0; detA < full_res_det_num; detA++) 
         { 
             for (uint layerA = 0; layerA < full_res_layer_num; layerA++) 
             { 
+                std::cout << detA << ",\t" << ringA << "\n";
                 for(uint ringB = 0; ringB < full_res_ring_num; ringB++) 
                 { 
                     for(uint detB = 0; detB < full_res_det_num; detB++) 
                     {  
                         for (uint layerB = 0; layerB < full_res_layer_num; layerB++) 
                         { 
-                            if(detA==detB && ringA==ringB){continue;}; 
+                            if(detA==detB && ringA==ringB && layerA==layerB){continue;}; 
                             stir::DetectionPosition<> full_res_det_pos1 = DetectionPosition(detA, ringA, layerA); 
                             stir::DetectionPosition<> full_res_det_pos2 = DetectionPosition(detB, ringB, layerB); 
                             full_res_det_pos_pair.pos2() = full_res_det_pos2; 
                             full_res_det_pos_pair.pos1() = full_res_det_pos1; 
                             Bin ful_res_b;
                             full_res_proj_data_info_blk_ptr->get_bin_for_det_pos_pair(ful_res_b, full_res_det_pos_pair);
-                            std::vector<uint> full_res_det1 = {detA, ringA, layerA}; 
-                            std::vector<uint> full_res_det2 = {detB, ringB, layerB}; 
-                            std::vector<uint> low_res_det1 = full_res_to_low_res_map[full_res_det1];
-                            std::vector<uint> low_res_det2 = full_res_to_low_res_map[full_res_det2];
-                            stir::DetectionPosition<> low_res_det_pos1 = DetectionPosition(low_res_det1[0], low_res_det1[1], low_res_det1[2]); 
-                            stir::DetectionPosition<> low_res_det_pos2 = DetectionPosition(low_res_det2[0], low_res_det2[1], low_res_det2[2]); 
-                            low_res_det_pos_pair.pos1()=low_res_det_pos1;
-                            low_res_det_pos_pair.pos2()=low_res_det_pos2;
-                            Bin low_res_b;
-                            low_res_proj_data_info_blk_ptr->get_bin_for_det_pos_pair(low_res_b, low_res_det_pos_pair);
-                            (*low_res_segments[low_res_b.segment_num()])[low_res_b.view_num()][low_res_b.axial_pos_num()][low_res_b.tangential_pos_num()] += ful_res_b.get_bin_value();
+                            if(ful_res_b.view_num() >= full_res_proj_data_info_sptr->get_min_view_num() 
+                                && ful_res_b.view_num() <= full_res_proj_data_info_sptr->get_max_view_num()
+                                && ful_res_b.axial_pos_num() >= full_res_proj_data_info_sptr->get_min_axial_pos_num(ful_res_b.segment_num())
+                                && ful_res_b.axial_pos_num() <= full_res_proj_data_info_sptr->get_max_axial_pos_num(ful_res_b.segment_num())
+                                && ful_res_b.tangential_pos_num() >= full_res_proj_data_info_sptr->get_min_tangential_pos_num()
+                                && ful_res_b.tangential_pos_num() <= full_res_proj_data_info_sptr->get_max_tangential_pos_num()){
+                                Sinogram<float> sino = full_resolution_proj_data_sptr->get_sinogram(ful_res_b.axial_pos_num(), ful_res_b.segment_num());
+                                if(full_res_sino[ful_res_b.view_num()][ful_res_b.view_num()] != 0.f){}
+                                //std::cout << "full res:\t" << ful_res_b.segment_num() << ", \t" << ful_res_b.view_num() << ",\t" << ful_res_b.axial_pos_num() << ",\t" << ful_res_b.tangential_pos_num() << ",\t" << full_res_sino[ful_res_b.view_num()][ful_res_b.view_num()] << "\n";
+                                std::vector<uint> full_res_det1 = {detA, ringA, layerA}; 
+                                std::vector<uint> full_res_det2 = {detB, ringB, layerB}; 
+                                std::vector<uint> low_res_det1 = full_res_to_low_res_map[full_res_det1];
+                                std::vector<uint> low_res_det2 = full_res_to_low_res_map[full_res_det2];
+                                stir::DetectionPosition<> low_res_det_pos1 = DetectionPosition(low_res_det1[0], low_res_det1[1], low_res_det1[2]); 
+                                stir::DetectionPosition<> low_res_det_pos2 = DetectionPosition(low_res_det2[0], low_res_det2[1], low_res_det2[2]); 
+                                low_res_det_pos_pair.pos1()=low_res_det_pos1;
+                                low_res_det_pos_pair.pos2()=low_res_det_pos2;
+                                Bin low_res_b;
+                                low_res_proj_data_info_blk_ptr->get_bin_for_det_pos_pair(low_res_b, low_res_det_pos_pair);
+                                (*low_res_segments[low_res_b.segment_num()])[low_res_b.view_num()][low_res_b.axial_pos_num()][low_res_b.tangential_pos_num()] = (*low_res_segments[low_res_b.segment_num()])[low_res_b.view_num()][low_res_b.axial_pos_num()][low_res_b.tangential_pos_num()] + full_res_sino[ful_res_b.view_num()][ful_res_b.view_num()];
+                                if((*low_res_segments[low_res_b.segment_num()])[low_res_b.view_num()][low_res_b.axial_pos_num()][low_res_b.tangential_pos_num()]!=0.f){std::cout << "low res:\t" << low_res_b.segment_num() << ", \t" << low_res_b.view_num() << ", \t" << low_res_b.axial_pos_num() << ", \t" << low_res_b.tangential_pos_num() << ",\t" << (*low_res_segments[low_res_b.segment_num()])[low_res_b.view_num()][low_res_b.axial_pos_num()][low_res_b.tangential_pos_num()] << "\n";}
+                            }
                         }
                     } 
                 } 
             } 
         } 
     } 
+    std::cout << "saving projdata\n";
     // save the low res proj data 
-    // only ProjData stores data
-    shared_ptr<ExamInfo> exam_info_sptr(new ExamInfo); 
-    shared_ptr<ProjData> tmp_sptr; 
-    tmp_sptr.reset(new ProjDataInterfile(exam_info_sptr, 
-    low_res_proj_data_info_blk_ptr->create_shared_clone(), 
-    "test_downsample.hs", 
-    std::ios::in | std::ios::out | std::ios::trunc)); 
-    for (int seg=start_segment_index; seg<=end_segment_index; seg++)
-    {
-        std::cout << "writing seg: " << seg << "\n" ;
-        tmp_sptr->set_segment(*low_res_segments[seg]);
-    }
     std::cout << "finish writing segment to projdata\n";
-    tmp_sptr->write_to_file("test.hs");
+    downsample_projdata_sptr->write_to_file("test.hs");
     return 0; 
 } 
  
